ea6ddb6baac47b95e689244e80ebf94008682eb0
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-04 22:50:34 -0600

    ejercicio1,terminado

--- Tarea_diccionario/Program.cs
+++ Tarea_diccionario/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-        
+            //ejercicio1
+        Dictionary<int,string> Estudiante = new Dictionary<int,string>();
+            Estudiante.Add(202618, "Maria");
+            Estudiante.Add(202620, "Juan");
+            Estudiante.Add(202619, "Carlos");
+            foreach (var item in Estudiante)
+            {
+                Console.WriteLine(item.Key + "-" + item.Value);
+            }
+            Console.WriteLine("Nombre del estudiante en especifico:_" + Estudiante[202620]);
+
+
+
+
+
+
         }
     }
 }
