e34ba410596d2216c35fbdf6ba91544dc438e628
moralesjk88-droid <moralesjk88@gmail.com>
2026-03-04 23:02:45 -0600

    Ejercicio3,terminado

--- Tarea_diccionario/Program.cs
+++ Tarea_diccionario/Program.cs
@@ -16,7 +16,17 @@
                 Console.WriteLine(item.Key + "-" + item.Value);
             }
 
+            //ejrcico3
 
+            Dictionary<string,double> Notas = new Dictionary<string,double>();
+            Notas.Add("Mario Rodas", 69.5);
+            Notas.Add("Eduardo Berquefer", 40);
+            Notas.Add("Alejandra Morales", 98.3);
+
+            if (Notas.ContainsKey("Eduardo Berquefer"))
+            {
+                Console.WriteLine("Nota del estudiante" + Notas["Eduardo Berquefer"]);
+            }
 
 
         }
